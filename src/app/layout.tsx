6cf50cf10b75e808a9ac5f3e50912cede6ab0792
Adawiyyah Latifah <adawiyyahlatifah@gmail.com>
2026-05-09 16:43:09 +0700

    add shadcn ui library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import "./globals.css";
 
 import { Inter } from "next/font/google";
 import QueryProvider from '@/providers/QueryProvider'
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${inter.variable}`}>
+    <html lang="en" className={cn("font-sans", inter.variable)}>
       <body>
         <QueryProvider>
           {children}
